b073f2acdb77e667f1e8c464ee1e6a6808d20a53
seooyxx <byunhwan8832@korea.ac.kr>
2023-03-26 15:59:10 +0900

    <feat> : write backend interface

--- place_recommender/app.py
+++ place_recommender/app.py
@@ -1,4 +1,5 @@
 import io
+import json
 import requests
 from flask import Flask, request, jsonify
 from function.feature_extraction import *
@@ -11,21 +12,17 @@ app = Flask(__name__)
 feature_model = FeatureExtractor()
 output_model = FeatureExtractor()
 recommender = ComputeSimilarity()
+
 api_key = 'AIzaSyAj6YPcapmjY-QjpThqYuhYIAoA84b6Oak'
 gmaps = googlemaps.Client(api_key)
+cloud_server_url = "http://34.22.71.66"
 
-# user_adderss and image_urls should be changed when there exits a event for user to upload the image and change the user_address. 
-user_address = '용산구 한강로동'
-image_urls = [
-    "https://storage.googleapis.com/homey-test-storage/0f6c85f6-84cc-41de-a3dc-c691a4a54a9d"
-]
-
-class PlaceInfo:
-    def __init__(self, title, address, picture, url):
-        self.title = title
-        self.address = address
-        self.picture = picture
-        self.url = url
+# class PlaceInfo:
+#     def __init__(self, title, address, picture, url):
+#         self.title = title
+#         self.address = address
+#         self.picture = picture
+#         self.url = url
 
 def download_image(url):
     img_bytes = requests.get(url).content
@@ -47,7 +44,7 @@ def get_top3_classes(top3_tensors):
 def get_nearby_places(top3_classes, user_address):
     results = []
     location = gmaps.geocode(user_address)[0]['geometry']['location']
-    cut = max(0, 4 - len(top3_classes)) if len(top3_classes) < 3 else 1
+    cut = max(0, 3 - len(top3_classes)) if len(top3_classes) < 3 else 1
     for idx, place in enumerate(top3_classes):
         places = gmaps.places_nearby(keyword=place,
                                      location=location,
@@ -61,23 +58,44 @@ def get_nearby_places(top3_classes, user_address):
                 photo_url = ''
 
             search_url = f'https://www.google.com/maps/search/?api=1&query=Google&query_place_id={place["place_id"]}'
-            place_info = PlaceInfo(title=place['name'],
-                                   address=place['vicinity'],
-                                   picture=photo_url,
-                                   url=search_url)
+
+            place_info = {
+                'title' : place['name'],
+                'address' : place['vicinity'],
+                'picture' : photo_url,
+                'url' : search_url
+            }
             results.append(place_info)
     return results
 
-@app.route('/')
-def predict():
-    # image_urls = request.args.getlist('image_url')
-    # user_address = request.args.get('user_address')
-    input_imgs = [download_image(url) for url in image_urls]
-    top3_tensors = get_top3_similar_images(image_urls)
-    top3_classes = get_top3_classes(top3_tensors)
-    nearby_places = get_nearby_places(top3_classes, user_address)
-    
-    return jsonify([vars(place) for place in nearby_places])
+# GET - /family/ids -> 서버에 저장된 가족 id 모두 가져오기
+@app.route('/family/ids', methods=['GET'])
+def get_family_ids():
+    response = requests.get(cloud_server_url + "/family/ids")
+    return response.json()["familyIds"]
 
+# GET - /photo/family/{id} -> 서버에 저장된 가족 갤러리 이미지 모두 가져오기
+@app.route('/photo/family/<id>', methods=['GET'])
+def get_family_info(id):
+    response = requests.get(cloud_server_url + "/photo/family/" + str(id))
+    response.encoding = 'UTF-8'
+    return response.json()['address'], response.json()['images']
+
+@app.route('/recommended-content/family-id/<id>', methods=['POST'])
+def generate_recommendations():
+    family_ids = get_family_ids()
+    for family_id in family_ids: 
+        address, image_urls = get_family_info(family_id)
+
+        # Run recommender
+        top3_tensors = get_top3_similar_images(image_urls)
+        top3_classes = get_top3_classes(top3_tensors)
+        nearby_places = get_nearby_places(top3_classes, address)
+        result_json = json.dumps(nearby_places) 
+               
+        response = requests.post(cloud_server_url + "/recommended-content/family-id/" + str(family_id), json=result_json)
+
+        return result_json, jsonify({"status": response.status_code})
+        
 if __name__ == '__main__':
     app.run(debug=True)
--- place_recommender/test.py
+++ place_recommender/test.py
@@ -1,86 +1,123 @@
-@app.route('/', methods=['GET'])
-def predict():
-    input_imgs = []
-    top3_tensors = []
-    top3_classes = []
-    results = []
-
-    # 주소와 이미지 URL을 GET 요청에서 가져옵니다
-    address = request.args.get('address')
-    image_urls = request.args.getlist('images')
-
-    # 이미지 URL을 이용하여 이미지를 로드하고 변환합니다
-    for url in image_urls:
-        img_bytes = requests.get(url).content
-        img = transform_image(img_bytes)
-        input_imgs.append(feature_model.forward(img))
-
-    top3_tensors = recommender.find_top3_similar(input_imgs)
-    for output in top3_tensors:
-        classes, probs = output_model.prediction(output)
-        top3_classes.append(classes)
-    top3_classes = list(set(top3_classes))
-
-    location = gmaps.geocode(address)[0]['geometry']['location']
-    cut = max(0, 4 - len(top3_classes)) if len(top3_classes) < 3 else 1
-
-    for idx, place in enumerate(top3_classes):
-        places = gmaps.places_nearby(keyword=place,
-                                     location=location,
-                                     radius=50000,
-                                     rank_by='prominence')
-
-        for place in places['results'][:cut]:
-            search_url = 'https://www.google.com/maps/search/?api=1&query=Google&query_place_id=' + place['place_id']
-            photo_url = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(place['photos'][0]['photo_reference'], api_key)
-            place_info = PlaceInfo(title=place['name'],
-                                   address=place['vicinity'],
-                                   picture=photo_url,
-                                   url=search_url)
-            results.append(place_info)
-    return jsonify([vars(place) for place in results])
-
-
 import io
 import requests
-import googlemaps
 from flask import Flask, request, jsonify
-from function.feature_extraction import FeatureExtractor
-from function.preprocessing import transform_image
-from function.similarity import ComputeSimilarity
+from function.feature_extraction import *
+from function.preprocessing import *
+from function.similarity import *
+import googlemaps
+feature_model = FeatureExtractor()
 
-app = Flask(__name__)
+test = [{"title": "Restaurant", "address": "Seoul, South Korea", "picture": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUjq9jlychiaIDAjFyqurFROnXnTdJfNV3R5ZDKC2MguhRLA0jAlCM9WjU8pAwD48UHNTy5QUqr3MikFxygQyzblMJkotI7jsu5eGG2iDhEZbovuwEGAmVCaVD1tzz_rLNj57gtJRq3nX48Gqd7EJemDirNlwEVexFnpRAgDCZVQtDgDeZYs&key=AIzaSyAj6YPcapmjY-QjpThqYuhYIAoA84b6Oak", "url": "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJcXYVN-GvfDURMfOCKkl4hB0"}, {"title": "Pizzeria O", "address": "86 Dongsung-gil, Jongno-gu", "picture": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUjq9jldbyiMWeV_qCy4sg1qNlnyu7lG3FCF_UAgpHGbnpk5VxWNS--CB8pujzAWuxQO73S8Nku5ddV927vGPg1CbcDCN0BhP6IgmSe7DzUSX6U367i96RWqDlUojBkstCpo6I7C_wfZ-4erhY0IwbyuPX1URDwkrAzgFBYloETbUxOopo7p&key=AIzaSyAj6YPcapmjY-QjpThqYuhYIAoA84b6Oak", "url": "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJ9Ye9LyyjfDURgYB4uqsmohw"}, {"title": "\uc608\ub78c\ub18d\uc6d0", "address": "649 \ub355\uc774\ub3d9 Ilsanseo-gu, Goyang-si", "picture": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUjq9jlniH2yJvWe6C98redIzTwdMDumoYZ_ayScjn1dN7zUp5jL2z_xYLIDKLWJI-Nbhb5AYmeVrPmVbVavyLQh6tkDS2wWwGTt1jToKREIOkF6EkSLVxNcXJ63ZMdfISGfCO3_rWLTSMujeigjLWdCl1sW7xPpiai_6Q3gfg8MTmO8b9vB&key=AIzaSyAj6YPcapmjY-QjpThqYuhYIAoA84b6Oak", "url": "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJrZqcvtGPfDURv14-oq2cnw4"}, {"title": "Coffee Shop", "address": "Oryu-dong, Seo-gu", "picture": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUjq9jlaFHp13wOABu1TAfyFCO1c9ln6dCoZDdrG7YD_6fyD9No5eGmHfCNnF78fB6pKPVDWO_G1tleQ6C2S063n53MAYUQQqkAcyOt1sMJMm035L_jThM2wQaFT7YVnuuMz8LuyBa1QPuXj5cXaTAcFY-pai0HJewNorZDEwwGwSXzznBmi&key=AIzaSyAj6YPcapmjY-QjpThqYuhYIAoA84b6Oak", "url": "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJ14fuavuBfDURW0l9Gi-UiL4"}]
 
-FEATURE_MODEL = FeatureExtractor()
-OUTPUT_MODEL = FeatureExtractor()
-RECOMMENDER = ComputeSimilarity()
-API_KEY = 'YOUR_API_KEY'
-GMAPS = googlemaps.Client(API_KEY)
+for a in test :
+    for key in a.keys():
+        print(key, a[key])
+    print('----------------------')
+# import requests
 
-USER_ADDRESS = '용산구 한강로동'
-IMAGE_URLS = [
-    "https://storage.googleapis.com/homey-test-storage/0f6c85f6-84cc-41de-a3dc-c691a4a54a9d"
-]
+# print("아아")
 
-class PlaceInfo():
-    def __init__(self, name: str, address: str, photo_url: str, search_url: str):
-        self.name = name
-        self.address = address
-        self.photo_url = photo_url
-        self.search_url = search_url
+# localUrl = "http://127.0.0.1:8080/ping"
+# cloudServerUrl = "http://34.22.71.66/ping"
+# response = requests.get(cloudServerUrl)
 
-@app.route('/')
-def predict():
-    input_images = []
-    top3_tensors = []
-    top3_classes = []
-    results = []
-        
-    for image_url in IMAGE_URLS:
-        image_bytes = requests.get(image_url).content
-        image = transform_image(image_bytes)
-        input_images.append(FEATURE_MODEL.forward(image))
-    
-    top3_tensors = RECOMMENDER.find_top3_similar(input_images)
-    for tensor in top3_tensors:
-        classes, probabilities = OUTPUT_MODEL.prediction(tensor
+# print(response.text)
+
+# # Todo
+# # 1. GET - /family/ids -> 가족 id 모두 가져오기
+# # 3. 모든 가족에 대해서 다음을 반복
+# #   3-1. GET - /photo/family/{id} -> 가족 갤러리 이미지 모두 가져오기
+# #   3-2. 모델 돌려서 추천 정보 만들기
+# #   3-3. 원하는 형태로 가공
+# #   3-4. POST - /recommended-content/family-id/{id} -> 추천 정보 저장하기
+
+# response = requests.get("http://34.22.71.66/family/ids")
+# print(response.json()["familyIds"])
+# familyIdList = response.json()["familyIds"];
+
+
+# for x in familyIdList :
+#     photoResponse = requests.get("http://34.22.71.66/photo/family/" + str(x))
+#     photoResponse.encoding = 'UTF-8'
+#     print("-----------{}------------".format(x))
+#     print(photoResponse.json())
+
+#     # Todo : 모델 돌려서 추천 정보 json formatting
+#     resultJson = {
+# 	    "title" : "test title",
+# 	    "address" : "test address",
+# 	    "picture" : "https://1234514",
+# 	    "url" : "https://rdgd"
+#     }
+
+#     recommendedContentPostResponse = requests.post("http://34.22.71.66/recommended-content/family-id/" + str(x), json=resultJson)
+#     print(recommendedContentPostResponse.status_code)   
+
+# Flask 서버가 구동된 상태에서 http://localhost:5000/family/ids 등의 API endpoint를 호출하면 해당 API endpoint의 함수가 실행됩니다. 이때, Flask는 해당 함수에서 반환하는 JSON 데이터를 HTTP 응답으로 변환하여 클라이언트에게 반환합니다.
+
+# 단, generate_recommendations 함수는 클라이언트에서 호출하는 것이 아니라 서버에서 주기적으로 실행되어야 합니다. 따라서 이 함수는 별도의 스케줄링 작업 등을 통해 주기적으로 호출되도록 설정해야 합니다.
+
+
+
+# from flask import Flask, jsonify, request
+# import requests
+
+# app = Flask(__name__)
+# cloud_server_url = "http://34.22.71.66"
+
+# # GET - /family/ids -> 서버에 저장된 가족 id 모두 가져오기
+# @app.route('/family/ids', methods=['GET'])
+# def get_family_ids():
+#     response = requests.get(cloud_server_url + "/family/ids")
+#     return jsonify(response.json())
+
+# # GET - /photo/family/{id} -> 서버에 저장된 가족 갤러리 이미지 모두 가져오기
+# @app.route('/photo/family/<id>', methods=['GET'])
+# def get_family_photos(id):
+#     response = requests.get(cloud_server_url + "/photo/family/" + str(id))
+#     return jsonify(response.json())
+
+# # POST - /recommended-content/family-id/{id} -> 추천 정보 서버에 저장하기
+# @app.route('/recommended-content/family-id/<id>', methods=['POST'])
+# def save_recommended_content(id):
+#     content = request.json
+#     response = requests.post(cloud_server_url + "/recommended-content/family-id/" + str(id), json=content)
+#     return jsonify({"status": response.status_code})
+
+# # 모든 가족에 대해서 추천 정보 생성 후 저장
+# @app.route('/generate-recommendations', methods=['POST'])
+# def generate_recommendations():
+#     family_id_list = requests.get(cloud_server_url + "/family/ids").json()['familyIds']
+
+#     for family_id in family_id_list:
+#         photos = requests.get(cloud_server_url + "/photo/family/" + str(family_id)).json()
+
+#         # Todo : 모델 돌려서 추천 정보 json formatting
+#         result_json = [
+#             {
+#                 "title": "test title",
+#                 "address": "test address",
+#                 "picture": "https://1234514",
+#                 "url": "https://rdgd"
+#             },
+#             {
+#                 "title": "test title3",
+#                 "address": "test address",
+#                 "picture": "https://1234514",
+#                 "url": "https://rdgd"
+#             },
+#             {
+#                 "title": "test title2",
+#                 "address": "test address",
+#                 "picture": "https://1234514",
+#                 "url": "https://rdgd"
+#             }
+#         ]
+
+#         recommended_content_post_response = requests.post(cloud_server_url + "/recommended-content/family-id/" + str(family_id), json=result_json)
+
+#     return jsonify({"status": "success"})
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
